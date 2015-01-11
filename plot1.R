7aa37c60fa03171efad8ab3c06839e9d082100f6
aprGitHub <apr.workdir@gmail.com>
2015-01-11 18:26:52 +0100

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -2,6 +2,8 @@
 # =================
 # Specifications:
 # Construct the plot and save it to a PNG file with a width of 480 pixels and a height of 480 pixels. 
+# png(file = outFile_png, width = 480, height = 480, units = "px") # actually these are the with and height features by default
+
 
 
 # Load the subFile into an R object
@@ -9,7 +11,7 @@ subFile <- "data/household_power_consumption.subset.csv"
 data <- read.csv(subFile, sep=";", header=TRUE)
 
 # Create the plot and save it into a png file
-outFile_png="data/plot1.png"
+outFile_png="plot1.png"
 png(file = outFile_png) ## Open the PNG device. Create plot and send to a file (no plot will appear on the screen:-(
 hist(data$Global_active_power,xlab="Global Active Power (kilowats)",main="Global Active Power",col="red",ylim=c(0,1200)) 
 dev.off() ## Close the PDF file device
